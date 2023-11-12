795d0d6f95be83654f2187ab71d8170e5d789c5c
ziho <ziho.dev@gmail.com>
2023-11-12 23:31:23 +0900

    feat: giscus

---
+++ src/components/Comments.tsx
@@ -0,0 +1,19 @@
+import Giscus from "@giscus/react";
+
+export function Comments() {
+  return (
+    <Giscus
+      id="Comments"
+      repo="Zih0/ziho.dev"
+      repoId="R_kgDOKseOxA"
+      category="Announcements"
+      categoryId="DIC_kwDOKseOxM4Ca5Gl"
+      mapping="pathname"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="top"
+      theme="preferred_color_scheme"
+      lang="ko"
+    />
+  );
+}
