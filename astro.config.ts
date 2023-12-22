25c159e1a099a662d92e4d34507b81a0ae419deb
Jiho Shin <ziho.dev@gmail.com>
2023-12-22 17:47:13 +0900

    astro v4 migration (#5)
    * Revert "Revert "astro v4" (#4)"
    
    This reverts commit 8277b22d0fcd66299eb59e72716b2687c42fa3f7.
    
    * astrojs/check v-up
    
    * fix: rehypePlugin type
    
    * rehype-pretty-code v-up
    
    * deps v up

--- astro.config.ts
+++ astro.config.ts
@@ -9,16 +9,8 @@ import remarkToc from "remark-toc";
 import rehypeExternalLinks from "rehype-external-links";
 import rehypeSlug from "rehype-slug";
 import rehypeAutoLinkHeadings from "rehype-autolink-headings";
-import rehypePrettyCode, {
-  type Options as PrettyCodeOptions,
-} from "rehype-pretty-code";
-import moonlightTheme from "./public/code-block-theme/moonlight-ii.json" assert { type: "json" };
-
-const prettyCodeOption: PrettyCodeOptions = {
-  // note: Options['theme'] 타입이 CustomTheme을 제대로 지원안함
-  // @ts-ignore
-  theme: moonlightTheme,
-};
+import rehypePrettyCode from "rehype-pretty-code";
+import moonlightTheme from "./public/code-block-theme/moonlight-ii.json";
 
 // https://astro.build/config
 export default defineConfig({
@@ -40,7 +32,12 @@ export default defineConfig({
     syntaxHighlight: false,
     remarkPlugins: [remarkToc],
     rehypePlugins: [
-      [rehypePrettyCode, prettyCodeOption],
+      [
+        rehypePrettyCode as RehypePrettyCodeType,
+        {
+          theme: moonlightTheme,
+        },
+      ],
       rehypeSlug,
       rehypeAutoLinkHeadings,
       [rehypeExternalLinks, { target: "_blank", rel: "noopener noreferrer" }],
@@ -53,3 +50,8 @@ export default defineConfig({
   },
   scopedStyleStrategy: "where",
 });
+
+// FIXME: astro v4 rehypePlugin type이 변경되면서 깨지는 현상 발생
+type RehypePrettyCodeType = (
+  options?: Parameters<typeof rehypePrettyCode>[0]
+) => undefined;
